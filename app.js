110fab6825442b105f2e9bd0a5bd13db6336e293
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-03-01 11:18:10 +0600

    modal added

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const loadPhones = async(searchText) =>{
+const loadPhones = async(searchText, dataLimit) =>{
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
 
-const displayPhones = phones =>{
+const displayPhones = (phones, dataLimit) =>{
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
 
 
-    // display 5phone only
-    phones = phones.slice(0,5);
+    // display 10phone only
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+      phones = phones.slice(0,10);
+      showAll.classList.remove('d-none');
+    }
+    else{
+      showAll.classList.add('d-none');
+    }
+    
 
     // display no phone
     const noPhone = document.getElementById('warning-text-area');
@@ -41,29 +49,83 @@ const displayPhones = phones =>{
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailsModal">Show Details</button>
+           
               </div>
             </div>
         `
         phonesContainer.appendChild(phoneDiv);
-    })
+    });
+    // stop loader
+    toggleSpinner(false);
 }
 
 
+const processSearch = (dataLimit) =>{
+    // start loader
+    toggleSpinner(true)
+
+    const searchField = document.getElementById('inputText');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+
+// handle search button click
 document.getElementById('btn-search').addEventListener('click', function(){
-
-const searchField = document.getElementById('inputText');
-const searchText = searchField.value;
-loadPhones(searchText);
-
-
-
-
-
-
+  processSearch(10);
 
 } )
 
+// handle search input field enter kye
+document.getElementById('inputText').addEventListener("keyup", function(event){
+    if (event.key === 'Enter') {
+        document.getElementById("btn-search").click();
+    }
+});
 
+
+const toggleSpinner = isLoading =>{
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+    loaderSection.classList.remove('d-none');
+  }
+  else{
+    loaderSection.classList.add('d-none');
+  }
+}
+
+
+
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function(){
+
+  processSearch();
+
+})
+
+const loadPhoneDetails = async(id) =>{
+const url = `https://openapi.programming-hero.com/api/phone/${id}`
+const res = await fetch(url);
+const data = await res.json();
+displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = (phone) =>{
+console.log(phone);
+const phoneNameTitle = document.getElementById('phoneDetailsModalLabel');
+phoneNameTitle.innerText = `${phone.name}`;
+
+const modalBody = document.getElementById('modal-body-content');
+modalBody.innerHTML = `
+<img src="${phone.image}" alt="">
+<h5>Brand:${phone.brand}</h5>
+<p>Release Date:${phone.releaseDate}</p>
+
+
+`
+
+}
 
 
 
